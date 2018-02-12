f243bdab6191ff67e4bc830674d847f93059886b
Sunny Thakur <sunny.t@puppetmaster.meradnsserver.com>
2018-02-12 21:57:00 +0530

    Modifying run-puppet.sh script

--- files/run-puppet.sh
+++ files/run-puppet.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-cd /etc/puppetlabs/code/environments/production && git pull /opt/puppetlabs/bin/puppet apply manifests/
+cd /etc/puppetlabs/code/environments/production && git pull 
+/opt/puppetlabs/bin/puppet apply manifests/
