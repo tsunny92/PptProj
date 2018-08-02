90a6b4ffb9fed70d53f9fcbd3644b310c7642a6d
Sunny <sunny@localhost.localdomain>
2018-08-03 02:12:24 +0530

    Add puppet files demo

---
+++ Puppetdemo/hash.pp
@@ -0,0 +1,6 @@
+$heights = {
+	'john' => 160,
+	'smith' => 168,
+	'alex' => 170,
+}
+notice("Johns height is ${heights['john']} cm")
---
+++ Puppetdemo/newfacts.pp
@@ -0,0 +1,2 @@
+$myip = $facts['networking']['interfaces']['ens33']['ip']
+notice("IP address is $myip")
